00001ef614b7473d0eae5623919412bede38f3c7
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-31 15:29:16 +0530

    [Raj]Add: Return all permutations using iterative and recursi

--- Algorithm/Algorithm/Program.cs
+++ Algorithm/Algorithm/Program.cs
@@ -5,11 +5,23 @@ namespace AlgorithmProblem
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("\nPress 1 . Insertion Sort");
+            Console.WriteLine("\nPress 1.to Calculate Permutation using Recursion &Iterative Method of a string" +
+                "\n2.Insertion Sort");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
                 case 1:
+                    Console.WriteLine("Enter a string to calculate Permutation");
+                    string str = Console.ReadLine();
+                    int n = str.Length;
+                    Console.WriteLine("Permutation through Recursion Method");
+                    Algorithm.RecursivePermutation(str, 0, n - 1);
+                    Console.WriteLine("\nPermutation through Iteration Method");
+                    string str1 = Console.ReadLine();
+                    int n1 = str1.Length;
+                    Algorithm.IterativePermutation(str1, n1);
+                    break;
+                case 2:
                     Console.WriteLine("Enter the Array size");
                     int number = Convert.ToInt32(Console.ReadLine());
                     List<int> array = new List<int>();
